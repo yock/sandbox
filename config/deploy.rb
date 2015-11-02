bf72e79bc01ae462d91b0cfca0316febad47b3d0
Michael Yockey <mike@heysparkbox.com>
2015-11-02 11:36:10 -0500

    Linking secrets during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,17 @@ set :application, 'Sandbox'
 set :repo_url, 'git@github.com:yock/sandbox.git'
 set :deploy_to, '/var/www/html/sandbox.sparkboxqa.com'
 
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        execute :rake, 'cache:clear'
+        execute :touch, 'tmp/restart.txt'
+      end
+    end
+  end
+
+end
