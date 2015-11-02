00485bc89e8269c7100caac3ee0a9a3430137b91
Michael Yockey <mike@heysparkbox.com>
2015-11-02 11:43:47 -0500

    Adding form to set wait time

--- app/controllers/wait_controller.rb
+++ app/controllers/wait_controller.rb
@@ -1,10 +1,17 @@
 class WaitController < ApplicationController
   def index
-    @delay = params[:delay].to_i
-    @start_time = Time.zone.now
-    Rails.logger.info "Beginning #{@delay} second wait at #{@start_time.to_s}"
-    Kernel.sleep(@delay)
-    @end_time = Time.zone.now
-    Rails.logger.info "Ending #{@delay} second wait at #{@end_time.to_s}"
+    if delayed_response?
+      @delay = params[:delay].to_i
+      @start_time = Time.zone.now
+      Rails.logger.info "Beginning #{@delay} second wait at #{@start_time.to_s}"
+      Kernel.sleep(@delay)
+      @end_time = Time.zone.now
+      Rails.logger.info "Ending #{@delay} second wait at #{@end_time.to_s}"
+    end
   end
+
+  def delayed_response?
+    params[:delay].present?
+  end
+  helper_method :delayed_response?
 end
